92171fea744668945a7f5c1070b7fdf2021d2ca7
Aaron Chu <xweichu@hotmail.com>
2019-12-05 20:09:31 -0800

    added more operations to example3

--- examples/example3.py
+++ examples/example3.py
@@ -3,4 +3,11 @@ sk = SkyhookDM()
 sk.connect('localhost')
 urls = ['https://github.com/uccross/skyhookdm-pythonclient/blob/master/nano_aod.root?raw=true']
 sk.writeDataset(urls,'nanoexample')
-sk.getDataset('nanoexample')
+dst = sk.getDataset('nanoexample')
+files = dst.getFiles()
+file = files[0]
+schema = file.getSchema()
+rt = file.getRoot()
+children = rt.getChildren()
+file.getAttributes()
+table = sk.runQuery(f,'select event>X, project Events;1.Muon_dzErr,Events;1.SV_x,Events;1.Jet_puId,Events;1.HLT_AK8PFHT900_TrimMass50,Events;1.FatJet_n3b1')
