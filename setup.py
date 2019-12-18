19bc62fd9969563306139ec5eedc166f64b88748
Aaron Chu <xweichu@hotmail.com>
2019-12-17 22:32:29 -0800

    updated url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Xiaowei Chu',                   # Type in your name
   author_email = 'xweichu@hotmail.com',      # Type in your E-Mail
   url = 'https://github.com/uccross/skyhookdm-pythonclient',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/uccross/skyhookdm-pythonclient/archive/v1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/uccross/skyhookdm-pythonclient/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['SkyhookDM', 'Skyhook', 'Skyhook-pythonclient'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pyarrow',
