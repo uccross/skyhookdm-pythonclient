70bc91291ea5e7a221d4bd933aeb519db9355110
Aaron Chu <xweichu@hotmail.com>
2019-12-17 16:58:27 -0800

    added example5

---
+++ examples/example5.py
@@ -0,0 +1,8 @@
+# suppose 
+from skyhook import SkyhookDM
+sk = SkyhookDM()
+sk.connect('128.105.144.228')
+dst = sk.getDataset('nanodst')
+tables = sk.runQuery(dst,'select *, project Events;1.nMuon, Events;1.Muon_pt, Events;1.Muon_eta, Events;1.Muon_mass, Events;1.Muon_charge')
+dataframe = tables[0].to_pandas()
+print(dataframe)
