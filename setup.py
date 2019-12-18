d0fe51572b5cc5b8a809031f5243bc69ccd7d0ba
Aaron Chu <xweichu@hotmail.com>
2019-12-17 22:51:11 -0800

    modified structure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'skyhookdm-pythonclient',         # How you named your package folder (MyLib)
-  packages = ['skyhookdm-pythonclient'],   # Chose the same as "name"
+  name = 'skyhookdm',         # How you named your package folder (MyLib)
+  packages = ['skyhookdm'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python client of Skyhook for Ceph',   # Give a short description about your library
