335a3ec97ee2a5291c3b17bfe008fa4aabaeedb2
Aaron Chu <xweichu@hotmail.com>
2019-12-17 22:29:18 -0800

    added cfg and license

--- setup.py
+++ setup.py
@@ -8,20 +8,22 @@ setup(
   author = 'Xiaowei Chu',                   # Type in your name
   author_email = 'xweichu@hotmail.com',      # Type in your E-Mail
   url = 'https://github.com/uccross/skyhookdm-pythonclient',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  download_url = 'https://github.com/uccross/skyhookdm-pythonclient/archive/v1.0.tar.gz',    # I explain this later on
+  keywords = ['SkyhookDM', 'Skyhook', 'Skyhook-pythonclient'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'pyarrow',
+          'uproot',
+          'uproot',
+          'bokeh',
+          'wget',
+          'backports.lzma',
+          'dask[complete]',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 2.7',      #Specify which pyhton versions that you want to support
   ],
 )
