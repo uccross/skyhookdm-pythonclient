92145892a05ed691dadfa00cd6324c8e797303b9
Aaron Chu <xweichu@hotmail.com>
2019-12-20 01:00:37 -0800

    Update startup.sh

--- skyhookdmpy/startup.sh
+++ skyhookdmpy/startup.sh
@@ -2,4 +2,6 @@ pkill screen
 sleep 1
 screen -d -m dask-scheduler --host localhost
 screen -d -m dask-worker localhost:8786 --nthreads 20
-echo "SkyhookDM started!"
+ps -C dask-scheduler >/dev/null && echo "dask-scheduler is running." || echo "ERROR:dask-scheduler is not running!"
+ps -C dask-worker >/dev/null && echo "dask-worker is running." || echo "ERROR:dask-worker is not running!"
+echo "Done!"
