c2a6b94c3c7d6bff30116822528853df9a242014
Aaron Chu <xweichu@hotmail.com>
2019-12-22 20:00:12 -0800

    moved requirments.sh / startup.sh to root folder

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 pkill screen
 sleep 1
-screen -d -m dask-scheduler --host localhost
-screen -d -m dask-worker localhost:8786 --nthreads 20
+ip_addr=$(ifconfig eno1 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://');
+screen -d -m dask-scheduler --host $ip_addr
+screen -d -m dask-worker $ip_addr:8786 --nthreads 20
 ps -C dask-scheduler >/dev/null && echo "dask-scheduler is running." || echo "ERROR:dask-scheduler is not running!"
 ps -C dask-worker >/dev/null && echo "dask-worker is running." || echo "ERROR:dask-worker is not running!"
 echo "Done!"
