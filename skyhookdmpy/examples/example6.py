1da5215e2b76fb131986a57d7256ebf791a67007
Aaron Chu <xweichu@hotmail.com>
2020-01-16 22:58:22 -0800

    Fixed wrong name of the dst.

--- skyhookdmpy/examples/example6.py
+++ skyhookdmpy/examples/example6.py
@@ -3,7 +3,7 @@ sk = SkyhookDM()
 sk.connect('128.105.144.150')
 urls = ['/mnt/sda4/Run2012B_DoubleMuParked.root','/mnt/sda4/Run2012C_DoubleMuParked.root']
 sk.writeDataset(urls,'demodst')
-dst = sk.getDataset('exampledst')
+dst = sk.getDataset('demodst')
 tables = sk.runQuery(dst,'select *, project Events;1.nMuon, Events;1.Muon_pt, Events;1.Muon_eta, Events;1.Muon_mass, Events;1.Muon_charge')
 dataframe = tables[0].to_pandas()
 print(dataframe)
