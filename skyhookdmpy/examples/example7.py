ac18c83bbfb60bb8df42c3697210130b742f90fa
Aaron Chu <xweichu@hotmail.com>
2020-01-16 22:03:56 -0800

    added an example to demo the wirteArrowTable() function

---
+++ skyhookdmpy/examples/example7.py
@@ -0,0 +1,23 @@
+import pyarrow as pa
+import pandas as pd
+
+# Setup SkyhookDM
+from skyhookdmpy import SkyhookDM
+
+# Create a new SkyhookDM object
+sk = SkyhookDM()
+
+# Connect to the skyhook driver given the IP 
+sk.connect('128.105.144.150')
+
+# Create a panda dataframe with one column named 'a'
+df = pd.DataFrame({"a": [1, 2, 3]})
+
+# Convert the panda dataframe to arrow table. 
+table = pa.Table.from_pandas(df)
+
+# Write the arrow table to the Ceph cluster.
+# This function is under development and it should accept more arguments such as metadata of the table. 
+sk.writeArrowTable(table,'tname')
+
+# Query functions of SkyhookDM are not compatible with the data written by using the writeArrowTable() function for now. 
