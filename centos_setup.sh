cb9dc2a4b2a499d2b3ffc980f00ddee9391496bd
Aaron Chu <xweichu@hotmail.com>
2020-02-25 12:41:00 -0800

    added a script for centos

---
+++ centos_setup.sh
@@ -0,0 +1,6 @@
+# To be further tested on new machines.
+sudo pip install numpy==1.14.6
+sudo pip install --upgrade setuptools
+sudo pip install --upgrade pip
+sudo pip install --upgrade --force-reinstall pip==9.0.3
+sudo pip install skyhookdmpy --disable-pip-version-check
