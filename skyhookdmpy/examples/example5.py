b81a3100c61007450c686e8632c52e684e7fe908
Aaron Chu <xweichu@hotmail.com>
2020-03-17 12:42:40 -0700

    Added the 'hepdatapool' para

--- skyhookdmpy/examples/example5.py
+++ skyhookdmpy/examples/example5.py
@@ -1,7 +1,7 @@
 # suppose 
 from skyhookdmpy import SkyhookDM
 sk = SkyhookDM()
-sk.connect('128.105.144.228')
+sk.connect('ipaddr', 'hepdatapool')
 dst = sk.getDataset('nanodst')
 tables = sk.runQuery(dst,'select *, project Events;1.nMuon, Events;1.Muon_pt, Events;1.Muon_eta, Events;1.Muon_mass, Events;1.Muon_charge')
 dataframe = tables[0].to_pandas()
