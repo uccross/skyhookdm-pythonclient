db4f36db7df80c498216662fc23849e1b8d998f1
Aaron Chu <xweichu@hotmail.com>
2020-03-17 12:43:17 -0700

    Update example2.py

--- skyhookdmpy/examples/example2.py
+++ skyhookdmpy/examples/example2.py
@@ -1,6 +1,6 @@
 from skyhookdmpy import SkyhookDM
 sk = SkyhookDM()
-sk.connect('localhost')
+sk.connect('localhost', 'hepdatapool')
 dst = sk.getDataset('mm')
 f = dst.getFiles()[0]
 table = sk.runQuery(f,'select *, project Events;1.Muon_dzErr,Events;1.SV_x,Events;1.Jet_puId,Events;1.HLT_AK8PFHT900_TrimMass50,Events;1.FatJet_n3b1')
